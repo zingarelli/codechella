c185775e545fa344cfdee754fcff9d00f346c670
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-03-01 17:58:01 -0300

    cleanup

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import styles from './Header.module.css';
 import logo from './logo.png';
 import { Link } from 'react-router-dom';
-import Experiencia from 'pages/Experiencia';
 
 export default function Header() {
     // close menu that is displayed in mobile layout
