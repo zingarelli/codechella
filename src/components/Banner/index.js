7a1463e17b3ec0069aac39904f1641ee41a9c218
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-03-02 13:27:12 -0300

    new prop: position

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,8 +1,14 @@
 import styles from './Banner.module.css';
 
-export default function Banner({ image, text=''}) {
+/**
+ * Renders an image in the background and a text (optional)
+ * @param {string} image - URL for an image 
+ * @param {string} position - how the image should be positioned (top, bottom, left, right, center). Default is center.
+ * @param {text} text - Optional text to be displayed in the middle. When added, opacity is applied to the background
+ */
+export default function Banner({ image, position='center', text=''}) {
     const backgroundImage = {
-        background: `url(${image}) no-repeat center/cover`
+        background: `url(${image}) no-repeat ${position}/cover`
     }
 
     // when there's a text, add opacity to the background image
