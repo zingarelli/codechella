f3a034042354f4f7946f42819a896de6d97dbeb9
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-03-03 11:01:22 -0300

    Page "Mapa"

--- src/pages/Mapa/index.js
+++ src/pages/Mapa/index.js
@@ -1,5 +1,55 @@
+import banner from './assets/banner.png';
+import mapa from './assets/mapa.png';
+import legenda from './assets/legenda.png';
+import pista from './assets/pista.png';
+import premium from './assets/premium.png';
+import cadeiras from './assets/cadeiras.png';
+import styles from './Mapa.module.css';
+import Banner from 'components/Banner';
+import Article from 'components/Article';
+
 export default function Mapa() {
     return (
-        <h1>Mapa</h1>
+        <>
+            <Banner image={banner} text='Mapa de Setores' position='bottom' />
+            <main>
+                <div className={styles.mapa}>
+                    <Article
+                        image={mapa}
+                        alt='mapa do evento, mostrando o posicionamento do palco, das pistas, cadeiras e rampas'
+                        title='Legenda'
+                    >
+                        <img src={legenda} alt="legenda para o mapa, indicado a cor para cada pista, cadeira e rampa" />
+                    </Article>
+                </div>
+                <h2>Mais detalhes sobre os setores:</h2>
+                <div className={styles.setores}>
+                    <Article
+                        image={pista}
+                        alt='garota com um bambolê em destaque, com várias outras pessoas ao fundo'
+                        imagePosition='top'
+                        title='Pista'
+                    >
+                        <p>Pista convencional, atendida pelos bares e banheiros das laterais do estádio. Espaço amplo, com local para sentar e descansar. Separada da pista premium por uma grade.</p>
+                    </Article>
+                    <Article
+                        image={premium}
+                        alt='várias pessoas de frente para um palco e algumas nuvens de fumaça amarelas, vermelhas e azul.'
+                        imagePosition='top'
+                        title='Pista Premium'
+                    >
+                        <p>Pista mais próxima do palco, com acesso muito próximo a banheiros e bares num raio de 100m. Todas as pessoas idosas e/ou com deficiência têm direito a acesso gratuito a essa área.</p>
+                    </Article>
+                    <Article
+                        image={cadeiras}
+                        alt='várias cadeiras reclináveis fechadas, da cor cinza, dispostas em diferentes níveis de altura'
+                        imagePosition='top'
+                        title='Cadeiras'
+                    >
+                        <p>Opção de cadeiras térreas ou elevadas. Acesso próximo a banheiros e bares, visão um pouco elevada em relação às pistas.</p>
+                    </Article>
+                </div>
+            </main>
+        </>
     )
 }
