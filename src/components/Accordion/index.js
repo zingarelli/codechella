976346def30b0d80f52b6fb2915cc776ca625ced
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-03-10 10:41:44 -0300

    animate arrow

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -6,22 +6,26 @@ import styles from './Accordion.module.css';
  * It can be used, for example, in a FAQ section.
  * @param {string} title - Title for the content
  */
-export default function Accordion({ title, children }) {
+export default function Accordion({ title, children }) { 
     // show/hide content
     // credits: https://www.w3schools.com/howto/howto_js_accordion.asp
     const toggleAccordion = (e) => {
         const divContainer = e.target.parentNode;
         const divContent = e.target.nextElementSibling;
+        const button = e.target;
 
         // if it has an inline style for height, content is already being displayed
         if (divContent.style.height) {
             divContent.style.height = null;
             divContainer.style.marginBottom = '8px';
+            // arrow is in a pseudo-element, needs to be styled inside the CSS file   
+            button.classList.remove(`${styles.accordion__opened}`);
         }
         else {
             // show content
             divContent.style.height = `${divContent.scrollHeight}px`;
             divContainer.style.marginBottom = '24px';
+            button.classList.add(`${styles.accordion__opened}`);
         }
     }
 
