1f7863765945883c5ba357676b301fdacfe1c20a
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-03-15 11:18:31 -0300

    new prop: onClick

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,9 @@ import styles from './Button.module.css';
 import { forwardRef } from 'react';
 
 // using forwardRef so other components can manipulate the created button
-const Button = forwardRef(({ image, children }, ref) => {
+const Button = forwardRef(({ image, onClick, children }, ref) => {
     return (
-        <button className={styles.button__container} ref={ref}>
+        <button className={styles.button__container} ref={ref} onClick={onClick}>
             {children}
             {image && <img src={image} alt="imagem decorativa do botão" />}
         </button>
