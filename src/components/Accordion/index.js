7c5d4e76fc1becf3818fd21c85fef0f4e9951774
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-03-20 16:39:42 -0300

    statefull Accordion

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './Accordion.module.css';
 
 /**
@@ -7,35 +8,41 @@ import styles from './Accordion.module.css';
  * @param {string} title - Title for the content
  */
 export default function Accordion({ title, children }) { 
+    const [active, setActive] = useState(false);
+    const accordionContent = useRef(null);
+    const accordionContainer = useRef(null);
+    const button = useRef(null);
+    
     // show/hide content
     // credits: https://www.w3schools.com/howto/howto_js_accordion.asp
-    const toggleAccordion = (e) => {
-        const divContainer = e.target.parentNode;
-        const divContent = e.target.nextElementSibling;
-        const button = e.target;
-
+    useEffect(() => {      
         // if it has an inline style for height, content is already being displayed
-        if (divContent.style.height) {
-            divContent.style.height = null;
-            divContainer.style.marginBottom = '8px';
-            // arrow is in a pseudo-element, needs to be styled inside the CSS file   
-            button.classList.remove(`${styles.accordion__open}`);
+        if (active) {
+            // show content
+            accordionContent.current.style.height = `${accordionContent.current.scrollHeight}px`;
+            accordionContainer.current.style.marginBottom = '24px';
+            // arrow is in a pseudo-element, needs to be styled inside the CSS file
+            button.current.classList.add(`${styles.accordion__open}`);
         }
         else {
-            // show content
-            divContent.style.height = `${divContent.scrollHeight}px`;
-            divContainer.style.marginBottom = '24px';
-            button.classList.add(`${styles.accordion__open}`);
+            accordionContent.current.style.height = null;
+            accordionContainer.current.style.marginBottom = '8px';               
+            button.current.classList.remove(`${styles.accordion__open}`);            
         }
+    }, [active])
+
+    // show/hide content
+    const toggleAccordion = (e) => {
+        setActive(!active);
     }
 
     return (
-        <div className={`${styles.accordion__container}`} >
-            <button className={styles.accordion__title} onClick={toggleAccordion}>{title}</button>
-            <div className={styles.accordion__content}>
-                <div className={styles.content__container}>
+        <div className={`${styles.accordion__container}`} ref={accordionContainer}>
+            <button className={styles.accordion__title} onClick={toggleAccordion} ref={button}>{title}</button>
+            <div className={styles.accordion__content} ref={accordionContent}>
+                {active && <div className={styles.content__container}>
                     {children}
-                </div>
+                </div>}
             </div>
         </div>
     )
