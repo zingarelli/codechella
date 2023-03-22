5e8704ec674d14b69dcde60a5b6570d94a1ccc09
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-03-22 10:25:13 -0300

    set inline styles based on state

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -10,36 +10,35 @@ import styles from './Accordion.module.css';
 export default function Accordion({ title, children }) { 
     const [active, setActive] = useState(false);
     const accordionContent = useRef(null);
-    const accordionContainer = useRef(null);
-    const button = useRef(null);
     
-    // show/hide content
+    // show/hide content by changing content's height
     // credits: https://www.w3schools.com/howto/howto_js_accordion.asp
-    useEffect(() => {      
-        // if it has an inline style for height, content is already being displayed
-        if (active) {
-            // show content
-            accordionContent.current.style.height = `${accordionContent.current.scrollHeight}px`;
-            accordionContainer.current.style.marginBottom = '24px';
-            // arrow is in a pseudo-element, needs to be styled inside the CSS file
-            button.current.classList.add(`${styles.accordion__open}`);
-        }
-        else {
-            accordionContent.current.style.height = null;
-            accordionContainer.current.style.marginBottom = '8px';               
-            button.current.classList.remove(`${styles.accordion__open}`);            
-        }
+    useEffect(() => {
+        accordionContent.current.style.height = active
+            ? `${accordionContent.current.scrollHeight}px`
+            : null;
     }, [active])
 
     // show/hide content
-    const toggleAccordion = (e) => {
-        setActive(!active);
+    const toggleAccordion = (e) => {        
+        setActive(oldstate => !oldstate);
     }
 
     return (
-        <div className={`${styles.accordion__container}`} ref={accordionContainer}>
-            <button className={styles.accordion__title} onClick={toggleAccordion} ref={button}>{title}</button>
-            <div className={styles.accordion__content} ref={accordionContent}>
+        <div 
+            className={`${styles.accordion__container}`}  
+            style={{marginBottom: active? '24px' : '8px'}}
+        >
+            <button 
+                className={`${styles.accordion__title} ${active? styles.accordion__open : ''}`} 
+                onClick={toggleAccordion} 
+            >
+                {title}
+            </button>
+            <div 
+                className={styles.accordion__content}
+                ref={accordionContent}
+            >
                 {active && <div className={styles.content__container}>
                     {children}
                 </div>}
