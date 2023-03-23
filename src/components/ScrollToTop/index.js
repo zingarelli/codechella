ed5a0c8121c806aa5158fbf3217312ffd14f58ab
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-03-23 16:00:09 -0300

    add comment with description

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -2,6 +2,11 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+/* 
+  Scroll to the top of the browser's window. This will be used everytime 
+  the user navigates to a new page to ensure that the user will view the 
+  top of the page when the page is rendered.
+*/
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
